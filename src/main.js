0faba577f4e2af84e9c000ac43fa3589400034da
marcelo_oliveira <f.marcelo.qxda@gmail.com>
2023-10-03 15:39:09 -0300

    Site para detectar palavras impróprias

---
+++ src/main.js
@@ -0,0 +1,75 @@
+import { palavroes } from "./modules/db_palavroes.js";
+
+const messageContent = document.getElementById("message-content");
+const messages = document.getElementById("messages");
+const getUserMessage = document.getElementById("button-submit");
+let finalMessage = "";
+
+function messageVerification() {
+  let initialMessage = messageContent.value || "Nenhuma mensagem digitada";
+  let messageForVerification = initialMessage
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase();
+
+  messageContent.value = "";
+
+  const regex = ["@", "#", "$", "&", "%", "*", "0", "1", "3", "4"];
+  const messageArr = messageForVerification.split(" ");
+
+  function strTransform(value, srtIndex) {
+    let num = value.length;
+    value = "";
+
+    for (let i = 0; i < num; i++) {
+      value += "*";
+    }
+
+    messageArr[srtIndex] = value;
+  }
+
+  palavroes.forEach((element) => {
+    messageArr.forEach((value, index) => {
+      let strValue = value.split("");
+      let isNumber = false;
+      let exist = false;
+
+      strValue.forEach((char) => {
+        regex.forEach((charInRegex) => {
+          if (char != charInRegex) return;
+          if (value.endsWith(".com")) return;
+          if (isNaN(value) == false) return;
+
+          strTransform(value, index);
+        });
+      });
+
+      if (!value.includes(element)) return;
+
+      strValue.filter((val) => (val == "-" ? (exist = true) : false));
+      if (exist) return;
+
+      strTransform(value, index);
+    });
+  });
+
+  const initialMessageArr = initialMessage.split(" ");
+  messageArr.forEach((str, index) => {
+    if (str.includes("**") == false) return;
+
+    initialMessageArr[index] = str;
+  });
+
+  finalMessage = initialMessageArr.reduce(
+    (acc, valorAtual) => acc + valorAtual + " ",
+    " "
+  );
+
+  messages.innerHTML += `<p class="message">${finalMessage}</p>`;
+}
+
+messageContent.addEventListener("keydown", (e) => {
+  if (e.code == "Enter") messageVerification();
+});
+
+getUserMessage.addEventListener("click", messageVerification);
---
+++ src/modules/db_palavroes.js
@@ -0,0 +1,73 @@
+const palavroes = [
+  "arrombad",
+
+  "baitol",
+  "bicha",
+  "biba",
+  "bolagato",
+  "boqueteir",
+  "boiola",
+  "boga",
+  "bost",
+  "bucet",
+
+  "cacet",
+  "carai",
+  "caralh",
+  "corn",
+  "cu",
+
+  "descgra",
+  "drogad",
+
+  "fiofo",
+  "fud",
+  "fudid",
+  "fdp",
+
+  "goso",
+  "gosad",
+
+  "imbeci",
+  "ignorant",
+
+  "nazist",
+
+  "otari",
+
+  "pau",
+  "penis",
+  "piroc",
+  "porra",
+  "porno",
+  "pqp",
+  "prostitu",
+  "punhet",
+  "put",
+
+  "quenga",
+
+  "raparig",
+  "retardad",
+  "rola",
+  "ruel",
+
+  "sexo",
+
+  "trepa",
+
+  "viad",
+  "vadi",
+  "vagabund",
+  "vagina",
+
+  "xavasca",
+  "xereca",
+  "xexec",
+  "xoxota",
+  "xavasc",
+
+  "69",
+];
+
+export { palavroes };
